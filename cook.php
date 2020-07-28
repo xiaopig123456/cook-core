ab5d9175de7b3069321cb6d7d5e63ad635ebfc14
朱言俊 <zhuyanjun@dookay.com>
2020-07-28 10:16:52 +0800

    初始化

--- cook.php
+++ cook.php
@@ -1,1 +1,22 @@
 <?php
+
+/**
+ * 全局函数
+ */
+
+/**
+ * 打印值 dump可传入多个参数
+ */
+function dump(){
+    $str = '';
+    $argList=func_get_args();
+    for($i=0;$i<func_num_args();$i++) {
+        $str .='<pre>'.\yii\helpers\VarDumper::dumpAsString($argList[$i]).'</pre>';
+    }
+    echo $str;
+
+}
+function dumpExit(){
+    dump(func_get_args());
+    exit;
+};
